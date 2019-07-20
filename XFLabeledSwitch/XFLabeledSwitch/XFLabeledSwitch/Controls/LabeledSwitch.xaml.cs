ded8a7063e74320fc55f37b2d436a46431a9d801
Sebastian Jensen <apps@tsjdev-apps.de>
2019-07-20 18:24:27 +0200

    - add labeledswitch control

---
+++ XFLabeledSwitch/XFLabeledSwitch/XFLabeledSwitch/Controls/LabeledSwitch.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFLabeledSwitch.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabeledSwitch : ContentView
+    {
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Title), typeof(string), typeof(LabeledSwitch));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+
+        public static readonly BindableProperty DetailProperty =
+            BindableProperty.Create(nameof(Detail), typeof(string), typeof(LabeledSwitch));
+
+        public string Detail
+        {
+            get => (string)GetValue(DetailProperty);
+            set => SetValue(DetailProperty, value);
+        }
+
+
+        public static readonly BindableProperty IsCheckedProperty =
+            BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(LabeledSwitch));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public LabeledSwitch()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnTapped(object sender, EventArgs e)
+        {
+            IsChecked = !IsChecked;
+        }
+    }
+}
